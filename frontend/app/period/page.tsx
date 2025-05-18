80626ca494d827c2c9456dca639ddc1f40cced6e
Juhun Park <riann3207@gmail.com>
2025-05-18 16:37:23 -0400

    fix(vercel): deployment issue 3rd attempt

--- frontend/app/period/page.tsx
+++ frontend/app/period/page.tsx
@@ -2,4 +2,6 @@ import { useState, useEffect } from "react";
 import { login, logout } from "@/lib/utils";
 import { useTheme } from "next-themes";
 
-export default function period() {}
+export default function Period() {
+    return <div className="p-8 text-2xl">Period Page</div>;
+}
--- frontend/app/roulette/page.tsx
+++ frontend/app/roulette/page.tsx
@@ -2,4 +2,6 @@ import { useState, useEffect } from "react";
 import { login, logout } from "@/lib/utils";
 import { useTheme } from "next-themes";
 
-export default function roulette() {}
+export default function Roulette() {
+    return <div className="p-8 text-2xl">Roulette Page</div>;
+}
